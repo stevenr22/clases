0188a690471f65e71f3cd4f71e84db358332694c
stevenr22 <rojasguerreros185@gmail.com>
2023-08-12 12:25:30 -0500

    hk

--- personaje.py
+++ personaje.py
@@ -1,39 +1,30 @@
-class personaje:
-   
-    
-    #para edirar creo  una clase init
-    def __init__(self,nombre,edad,estatura):
-        self.nombre = nombre
-        self.edad = edad
-        self.estatura = estatura
-        
-    def atributos(self):
-        print(f'Nombre: {self.nombre}')
-        print(f'edad: {self.edad}')
-        print(f'Estatura: {self.estatura}')
-        
-    def año_actual(self,edadDespues):
-        self.edad = self.edad+edadDespues
-        
-    def estatura_comprobacion(self):
-        if(self.estatura>0):
-            print(f'Tiene estatura')
-        elif (self.estatura<0):
-            print(f'No tiene estatura')
-        else:
-            print('No existe')
-        
-        
+def mostrar_menu():
+    print("Menú de opciones:")
+    print("1. Saludar")
+    print("2. Realizar cálculo")
+    print("3. Salir")
 
+def saludar():
+    nombre = input("Ingrese su nombre: ")
+    print("¡Hola, " + nombre + "!")
 
-#mando los valores q imprimo
-mi_personaje = personaje('Galo',22,23)
-#llamo los datos con la funcion atributos
-mi_personaje.atributos()
-print()
-#actualizo la edad
-mi_personaje.año_actual(56)
-#muestro la edad despues del tiempo establecido
-mi_personaje.atributos()
-print()
-print(mi_personaje.estatura_comprobacion())
+def realizar_calculo():
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    resultado = num1 + num2
+    print("El resultado de la suma es:", resultado)
+
+mostrar_menu()
+opcion = 0
+
+while opcion != 3:
+    opcion = int(input("Seleccione una opción (1/2/3): "))
+
+    if opcion == 1:
+        saludar()
+    elif opcion == 2:
+        realizar_calculo()
+    elif opcion == 3:
+        print("Saliendo...")
+    else:
+        print("Opción no válida. Intente nuevamente.")
--- vuelo.py
+++ vuelo.py
@@ -1,51 +1,83 @@
 class cliente:
 
     #inicializo los datos
-    def __init__(self,nombre,apellido,edad,ciudadania,celular,email,nomb_usu,clav_usu):
-        
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.ciudadania = ciudadania
-        self.celular = celular
-        self.email = email
+    def __init__(self,nomb_usu,clav_usu):
+   
         self.nomb_usu = nomb_usu
         self.clav_usu = clav_usu
    
 
 
         #creo la funcion atributos para mostrar los datos
-    def atributos(self):
-        print(f'Nombre: {self.nombre}')
-        print(f'Apellido: {self.apellido}')
-        print(f'Edad: {self.edad}')
-        print(f'Ciudadania: {self.ciudadania}')
-        print(f'Celular: {self.celular}')
-        print(f'Email: {self.email}')
+    def perfil(self):
+        #print(f'Nombre: {self.nombre}')
+        #print(f'Apellido: {self.apellido}')
+        #print(f'Edad: {self.edad}')
+        #print(f'Ciudadania: {self.ciudadania}')
+        #print(f'Celular: {self.celular}')
+        #print(f'Email: {self.email}')
         print(f'Nombre de usuario: {self.nomb_usu}')
         print(f'Contraseña: {self.clav_usu}')
 
+
     def iniciar_sesion(self,n_u,v_u):
+        
         self.n_u = n_u
         self.v_u = v_u
 
         if (self.nomb_usu == self.n_u) and (self.clav_usu == self.v_u):
-            print(f'Inicio exitoso, bienvenido señor {self.nombre}+{self.apellido}')
+            print(f'Inicio exitoso, bienvenido señor {self.nomb_usu}')
+            print("--------------------------------------------------")
+            print(f'Por favor, ingrese su conjunto de vuelo')
+            print()
+            print("1. VUELO DIRECTO\n2. VUELO TRANSVORDO\n3. Salir")
+            print()
+            opcion = 0
+        
+            while(opcion != 2):
+                opcion = int(input("Seleccione una opción (1/2/3)==> "))
+
+                if opcion == 1:
+                    print("AH ELEGIDO EL VUELO DIRECTO")
+              
+                    
+                elif opcion == 2:
+                    print("AH ELEGIDO EL VUELO TRANSVORDO")
+
+                elif opcion ==3:
+                    salir = input("¿Está seguro que desea salir? (s/n): ")
+                    if salir.lower() == "s":
+                        print("Saliendo...")
+                    else:
+                        print(f'Por favor, ingrese su conjunto de vuelo')
+                        print()
+                        print("1. VUELO DIRECTO\n2. VUELO TRANSVORDO\nSalir")
+                        print()
+                    
+              
+                    
+                else:
+                    print("OPCION INVALIDA")
+             
+            
+
         else:
             print(f'Usuario o contraseña incorrecta')
 
 #pido al usuario dichos datos
-nom = input("Ingrese el nombre: ")
-ape = input("Ingrese el apellido: ")
-edad = int(input("Ingrese el edad: "))
-ciuda = input("Ingrese el ciudadania: ")
-cel = int(input("Ingrese numero de celular: "))
-email = input("Ingrese el email: ")
+#nom = input("Ingrese el nombre: ")
+#ape = input("Ingrese el apellido: ")
+#edad = int(input("Ingrese el edad: "))
+#ciuda = input("Ingrese el ciudadania: ")
+#cel = int(input("Ingrese numero de celular: "))
+#email = input("Ingrese el email: ")
 nomb_u = input("Ingrese el nombre de usuario: ")
 clav_u = input("Ingrese el contraseña: ")
 #envio por parametros a la clase padre
-mi_cli = cliente(nom,ape,edad,ciuda,cel,email,nomb_u,clav_u)
+#mi_cli = cliente(nom,ape,edad,ciuda,cel,email,nomb_u,clav_u)
 #llamo  a la funcion atributos para visualizar los datos ingresados
+
+mi_cli=cliente(nomb_u,clav_u)
 mi_cli.iniciar_sesion('Srojas','123')
 
 
